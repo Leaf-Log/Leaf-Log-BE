3080c79c2f6be29b05927ddb9ed702bc2e1f64d4
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-10-01 00:32:22 +0900

    feat(#3): Profile 조회 기능 구현

---
+++ src/main/java/com/example/leaflog/bc/profile/profile/application/ProfileQueryService.java
@@ -0,0 +1,28 @@
+package com.example.leaflog.bc.profile.profile.application;
+
+import com.example.leaflog.bc.member.user.domain.User;
+import com.example.leaflog.bc.profile.profile.application.query.ProfileReadModel;
+import com.example.leaflog.bc.profile.profile.domain.Profile;
+import com.example.leaflog.bc.profile.profile.domain.repository.ProfileRepository;
+import com.example.leaflog.bc.profile.profile.presentation.exception.ProfileNotFoundException;
+import com.example.leaflog.bc.sharedkernel.user.provider.CurrentUserProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileQueryService {
+
+    private final CurrentUserProvider currentUserProvider;
+    private final ProfileRepository profileRepository;
+
+    @Transactional(readOnly = true)
+    public ProfileReadModel query(){
+        User user = currentUserProvider.getCurrentUser();
+        Profile profile = profileRepository.findById(user.userId())
+                .orElseThrow(ProfileNotFoundException::new);
+
+        return ProfileReadModel.from(user, profile);
+    }
+}
