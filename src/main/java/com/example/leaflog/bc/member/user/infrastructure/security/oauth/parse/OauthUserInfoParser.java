9d5b680c99b74a50a555ee020b94053609618ba6
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-10-01 11:00:08 +0900

    refactor(#6): 삭제

--- src/main/java/com/example/leaflog/bc/member/user/infrastructure/security/oauth/parse/OauthUserInfoParser.java
+++
@@ -1,9 +0,0 @@
-package com.example.leaflog.bc.member.user.infrastructure.security.oauth.parse;
-
-import com.example.leaflog.bc.member.user.infrastructure.security.oauth.dto.OauthUserInfo;
-
-import java.util.Map;
-
-public interface OauthUserInfoParser {
-    OauthUserInfo parse(Map<String, Object> attributes, String accessToken);
-}
