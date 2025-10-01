8531bc401686bbc3e2323dd967a3e6344fc9727f
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-10-01 11:02:31 +0900

    refactor(#6): RefreshToken 도메인 모델링

--- src/main/java/com/example/leaflog/bc/account/auth/domain/RefreshToken.java
+++ src/main/java/com/example/leaflog/bc/account/auth/domain/RefreshToken.java
@@ -1,28 +1,23 @@
-package com.example.leaflog.bc.member.user.infrastructure.refreshtoken;
+package com.example.leaflog.bc.account.auth.domain;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
 
-//refreshToken 은 순수 도메인 모델이 아니고 외부 모델에 가까움.
-//즉 DDD 모델링 대상이 아니라 판단.
-//추후 리펙토링
-@Getter
+
 @Builder
 @RedisHash
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class RefreshToken {
 
     @Id
     private String email;
 
+    @Getter
     @Indexed
     private String refreshToken;
 
@@ -32,5 +27,9 @@ public class RefreshToken {
     public void reissue(String newToken, Long newTtl){
         this.refreshToken = newToken;
         this.ttl = newTtl;
+    }
+
+    public String email(){
+        return email;
     }
 }
