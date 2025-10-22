63e3ba90aaa92f46b082cb08665713fd76551a49
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-10-22 09:10:43 +0900

    refactor(#12): TODO 주석 추가

--- src/main/java/com/example/leaflog/bc/account/auth/infrastructure/token/Token.java
+++ src/main/java/com/example/leaflog/bc/account/auth/infrastructure/token/Token.java
@@ -23,6 +23,9 @@ public class Token {
     @TimeToLive
     private Long ttl;
 
+    //GitHub Access Token이 이미 만료됐는데 Redis 에는 여전히 남아 있음 -> 401 Unauthorized
+    //TODO: 현재는 큰 문제가 발생하지 않기 때문에 유지, 하지만 실제 프러덕션 환경 때 고침
+    //TODO: AccessToken 암호화 추가
     private String githubAccessToken;
 
     public void reissue(String newToken, Long newTtl){
